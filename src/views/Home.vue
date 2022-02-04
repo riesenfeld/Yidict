1303bc519537eca74b895f86d38ef6a0387bbe8c
riesenfeld <aron.riesenfeld@gmail.com>
2022-02-03 17:48:34 -0800

    testing basic function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="fetchWords"></button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue"
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  methods: {
+    async fetchWords() {
+      const response = await fetch("/.netlify/functions/fetch-words").then((response) =>
+        response.json()
+      )
+      console.log(response)
+    },
+  },
 }
 </script>
