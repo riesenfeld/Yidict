64723351d3f7a36d5943b2f0f86acc0e36fee2be
riesenfeld <aron.riesenfeld@gmail.com>
2022-02-03 19:04:14 -0800

    continued testing

--- netlify/functions/fetch-words.js
+++ netlify/functions/fetch-words.js
@@ -1,8 +1,9 @@
 const words = require("../data/words.json")
 
 exports.handler = async function () {
+  console.log("YES")
   return {
     statusCode: 200,
-    body: JSON.stringify(words),
+    body: JSON.stringify(words.map((word) => word[0])),
   }
 }
