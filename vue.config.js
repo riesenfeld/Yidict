33d0d0fa5a73bdb28a7bfe7cec87052a409a0a1d
riesenfeld <aron.riesenfeld@gmail.com>
2022-02-22 14:04:04 -0800

    more experimentation

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,94 @@
-const path = require("path")
+var path = require("path")
+var webpack = require("webpack")
+var HtmlWebpackPlugin = require("html-webpack-plugin")
 const PrerenderSPAPlugin = require("prerender-spa-plugin")
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const VueLoaderPlugin = require("vue-loader/lib/plugin")
 
 module.exports = {
-  configureWebpack: () => {
-    //  if (process.env.NODE_ENV !== "production") return
-    return {
-      plugins: [
-        new PrerenderSPAPlugin(
-          // Absolute path to compiled SPA
-          path.resolve(__dirname, "dist"),
-          // List of routes to prerender
-          ["/", "/contact"],
-          {
-            // options
-          }
-        ),
-      ],
-    }
+  mode: process.env.NODE_ENV,
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/",
+    filename: "build.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: false,
+  },
+  devtool: "#eval-source-map",
+  plugins: [new VueLoaderPlugin()],
+}
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = "#source-map"
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      title: "Yidict",
+      template: "index.html",
+      filename: path.resolve(__dirname, "dist/index.html"),
+      favicon: "favicon.ico",
+    }),
+    new PrerenderSPAPlugin({
+      staticDir: path.join(__dirname, "dist"),
+      routes: ["/", "/contact"],
+
+      renderer: new Renderer({
+        // inject: {
+        //   foo: "bar",
+        // },
+        headless: true,
+        renderAfterDocumentEvent: "render-event",
+      }),
+    }),
+  ])
+} else {
+  // NODE_ENV === 'development'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"development"',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      title: "DEVELOPMENT Yidict",
+      template: "index.html",
+      filename: "index.html",
+      favicon: "favicon.ico",
+    }),
+  ])
 }
