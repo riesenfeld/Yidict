ef01d2f6c7e8fe8079ed2219cf91afc38830b8cf
riesenfeld <aron.riesenfeld@gmail.com>
2022-02-22 18:37:43 -0800

    testing contact form post with fetch for custom routing

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -6,18 +6,19 @@
     data-netlify-honeypot="bot-field"
     @submit.prevent="submitForm"
   >
-    <input type="hidden" name="form-name" id="honeypot" value="contact" />
+    <input type="hidden" name="form-name" id="honeypot" value="contact" v-model="honeypot" />
     <div class="form-item">
       <label for="subject">Subject:</label>
-      <input type="text" name="subject" id="subject" required />
+      <input type="text" name="subject" id="subject" v-model="subject" required />
     </div>
     <div class="form-item">
       <label for="message">Your message:</label>
-      <textarea name="message" id="message" cols="50" rows="10" required> </textarea>
+      <textarea name="message" id="message" cols="50" rows="10" v-model="message" required>
+      </textarea>
     </div>
     <div class="form-item">
       <label for="email">Your email address:</label>
-      <input type="email" name="email" id="email" required />
+      <input type="email" name="email" id="email" v-model="email" required />
     </div>
     <button class="form-item">Submit</button>
   </form>
@@ -26,9 +27,44 @@
 <script>
 export default {
   name: "ContactForm",
+  data() {
+    return {
+      honeypot: "notfilledout",
+      subject: "",
+      message: "",
+      email: "",
+    }
+  },
   methods: {
+    getEncoded() {
+      let obj
+      if (this.honeypot == "notfilledout") {
+        obj = {
+          "form-name": "contact",
+          subject: this.subject,
+          message: this.message,
+          email: this.email,
+        }
+      } else {
+        obj = {
+          "form-name": this.honeypot,
+          subject: this.subject,
+          message: this.message,
+          email: this.email,
+        }
+      }
+      return Object.keys(obj)
+        .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]))
+        .join("&")
+    },
     submitForm() {
-      this.$router.push("/contact?submitted=true")
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.getEncoded(),
+      })
+        .then(() => this.$router.push("/contact?submit=success"))
+        .catch(() => this.$router.push("/contact?submit=failure"))
     },
   },
 }
