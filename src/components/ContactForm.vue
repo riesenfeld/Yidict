581be109b29360eb247f4cbfcf575f02591ab1c0
riesenfeld <aron.riesenfeld@gmail.com>
2022-02-22 16:50:45 -0800

    separated contact form into its own component

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" id="honeypot" value="contact" />
+    <div class="form-item">
+      <label for="subject">Subject:</label>
+      <input type="text" name="subject" id="subject" required />
+    </div>
+    <div class="form-item">
+      <label for="message">Your message:</label>
+      <textarea name="message" id="message" cols="50" rows="10" required> </textarea>
+    </div>
+    <div class="form-item">
+      <label for="email">Your email address:</label>
+      <input type="email" name="email" id="email" required />
+    </div>
+    <button class="form-item">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.form-item {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2vh;
+  text-align: left;
+}
+label {
+  margin-bottom: 0.25vh;
+}
+textarea,
+input {
+  /* Use UA styles for form elements */
+  border: revert;
+}
+button {
+  background-color: var(--main-color);
+  color: white;
+  font-size: 1.2rem;
+  padding: 5px 8px 5px 8px;
+  border-radius: 5px;
+  box-shadow: 3px 3px 3px var(--shadow-color);
+}
+</style>
--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,27 +1,17 @@
 <template>
   <div id="contact-page">
-    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="form-name" id="honeypot" value="contact" />
-      <div class="form-item">
-        <label for="subject">Subject:</label>
-        <input type="text" name="subject" id="subject" required />
-      </div>
-      <div class="form-item">
-        <label for="message">Your message:</label>
-        <textarea name="message" id="message" cols="50" rows="10" required> </textarea>
-      </div>
-      <div class="form-item">
-        <label for="email">Your email address:</label>
-        <input type="email" name="email" id="email" required />
-      </div>
-      <button class="form-item">Submit</button>
-    </form>
+    <ContactForm />
   </div>
 </template>
 
 <script>
+import ContactForm from "@/components/ContactForm.vue"
+
 export default {
   name: "ContactPage",
+  components: {
+    ContactForm,
+  },
 }
 </script>
 
@@ -32,30 +22,5 @@ export default {
   justify-content: center;
   align-items: center;
   min-height: 90vh;
-}
-
-.form-item {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2vh;
-  text-align: left;
-}
-
-label {
-  margin-bottom: 0.25vh;
-}
-
-textarea,
-input {
-  /* Use UA styles for form elements */
-  border: revert;
-}
-button {
-  background-color: var(--main-color);
-  color: white;
-  font-size: 1.2rem;
-  padding: 5px 8px 5px 8px;
-  border-radius: 5px;
-  box-shadow: 3px 3px 3px var(--shadow-color);
 }
 </style>
